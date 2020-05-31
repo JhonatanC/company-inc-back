2058331c17d55a2647196a2fdd9cef3efc8b0427
Jhonatandev <jhonatanchurtado@gmail.com>
2020-05-31 15:30:13 -0500

    Creando rutas para consumir en la API y agregando permisos de CORS para evitar bloqueo del navegador

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -14,7 +14,7 @@ class ClienteController extends Controller
      */
     public function index()
     {
-        //
+        return Cliente::all();
     }
 
     /**
@@ -35,7 +35,8 @@ class ClienteController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $cliente = Cliente::create($request->all());
+        return $cliente;
     }
 
     /**
@@ -46,7 +47,7 @@ class ClienteController extends Controller
      */
     public function show(Cliente $cliente)
     {
-        //
+        return $cliente;
     }
 
     /**
@@ -69,7 +70,8 @@ class ClienteController extends Controller
      */
     public function update(Request $request, Cliente $cliente)
     {
-        //
+        $cliente->update($request->all());
+        return $cliente;
     }
 
     /**
@@ -80,6 +82,7 @@ class ClienteController extends Controller
      */
     public function destroy(Cliente $cliente)
     {
-        //
+        $cliente->delete();
+        return $cliente;
     }
 }
